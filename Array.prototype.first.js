42b90a64917652dba98031d239b4dee1a2d0de75
hansschmucker <hansschmucker@gmail.com>
2026-02-02 15:59:47 +0100

    Create Array.prototype.first.js

---
+++ Array.prototype.first.js
@@ -0,0 +1,11 @@
+/*
+    Returns the first element of an array with optional filtering and default value
+ */
+Array.prototype.first=function(filter,def){
+    if(filter)
+        var r=this.filter(filter);
+    else
+        var r=this;
+
+    return r.length?r[0]:def;
+};
