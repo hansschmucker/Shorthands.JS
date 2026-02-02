2b54ecaac081bdc7b8d02b291f59faa859a37754
hansschmucker <hansschmucker@gmail.com>
2026-02-02 16:16:51 +0100

    Create document.$.js

---
+++ documen.$.js
@@ -0,0 +1,9 @@
+/**
+* Returns an element matching the selector if the last part of the selector starts with #
+* Otherwise returns all elements matching the selector in the subtree as array.
+*/
+HTMLElement.prototype.$=Document.prototype.$=function(sel){
+  return /#[^\s\>~|]/.exec(sel)?
+    this.querySelector(sel):
+    [...this.querySelectorAll(sel)]
+};
