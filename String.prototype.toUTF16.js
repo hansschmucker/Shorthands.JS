4552a34a142384f43a05123a1bbf21c2f6cd20f0
hansschmucker <hansschmucker@gmail.com>
2026-02-06 11:22:58 +0100

    Create String.prototype.toUTF16.js

---
+++ String.prototype.toUTF16.js
@@ -0,0 +1,11 @@
+String.prototype.toUTF16=function(){
+    var out=new Uint8Array(2+this.length*2);
+    out[0]=0xFF;
+    out[1]=0xFE;
+    for(var i=0;i<this.length;i++){
+        var c=this.charCodeAt(i);
+        out[2+i*2]=c&0x00ff;
+        out[2+i*2+1]=(c&0xff00)>>>8;
+    }
+    return out;
+}
